9f974e46bc0be204e3fd54f5506a4e5ef7813cbc
Teerapat Anantarattanachai <zummon.space@gmail.com>
2023-04-17 05:42:24 +0000

    Update 5 files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "uikit/dist/css/uikit.min.css";
+import "uikit/dist/js/uikit.min.js";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
