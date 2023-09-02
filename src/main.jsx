2e6dc929529569ffeccccb74717b52cc9adde11d
Teerapat Anantarattanachai <zummon.space@gmail.com>
2023-09-02 03:30:10 +0000

    Update 12 files

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "uikit/dist/css/uikit.min.css";
-import "uikit/dist/js/uikit.min.js";
+import './style.css'
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
