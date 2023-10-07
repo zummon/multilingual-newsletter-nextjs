fa2a2062f9a2bab4b7cb8eb4a5711feb5dfc0338
zummon <zummon.space@gmail.com>
2023-10-07 14:27:04 +0700

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ export default function App() {
 			<div className="px-4 md:px-8 pb-7 md:pb-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 divide-y sm:divide-y-0 md:divide-x">
 				<div className="sm:row-span-2 md:row-span-2 p-2 md:p-3 lg:p-4">
 					<img
-						className="rounded"
+						className="rounded max-h-96 sm:max-h-none mx-auto"
 						src="https://cdn.leonardo.ai/users/e29c0123-9b74-49b3-a29b-9d7e230a889c/generations/d55e3601-4316-4aea-bbf0-4afaa9258273/variations/Default_Danielle_van_de_Donk_full_body_potrait_of_a_photoreali_0_d55e3601-4316-4aea-bbf0-4afaa9258273_1.jpg"
 						alt="pic"
 					/>
